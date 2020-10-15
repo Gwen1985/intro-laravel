0e5c94bfd4248f1b0423b0adfa0b6b5d4e8b53a6
gwen1985 <gwen.ellegeest@outlook.com>
2020-10-15 09:30:46 +0200

    Added 2 view files and an image folder.

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -1,1 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="G.WebDev&copy;" name="author">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Spicy Thai</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<body>
+
+<header>
+    <div class="row bg-dark text-white">
+        <div class="col-md d-flex justify-content-center">
+            <img src="../../public/image/spicy-thai.jpg" alt="picture">
+            <h1>Spicy Thai</h1>
+        </div>
+    </div>
+    <br>
+</header>
+
+<main>
+    <div class="container">
+
+        <div class="row p-4">
+
+        </div>
+
+    </div>
+
+</main>
+
+<footer class="row bg-dark text-white fixed-bottom">
+    <div class="col-md d-flex justify-content-center">
+        <p>Spicy Thai&copy;</p>
+    </div>
+</footer>
+
+</body>
+</html>
